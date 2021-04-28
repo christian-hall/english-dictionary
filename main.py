cb378b36c3757b9e27d3612c2788b8543bb27547
Christian Hall <christiannealhall@gmail.com>
2021-04-28 19:42:45 -0400

    first test of callin json

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# load attached json into a python dictionary
+import json
+
+print('Welcome to the English Dictionary Python Program')
+
+data = json.load(open('data.json', 'r'))
+
+
+def definition(entry):
+    return data[entry]
+
+
+word = input("Enter word: ")
+
+print(definition(word))
