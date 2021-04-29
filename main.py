2fef1904b0bbc87bc2aa479ef045c482244d2b7f
Christian Hall <christiannealhall@gmail.com>
2021-04-28 20:03:31 -0400

    added input validation

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
 
 print('Welcome to the English Dictionary Python Program')
+print('Enter "1234" at any time to quit the program')
 
-data = json.load(open('data.json', 'r'))
+word, data = '', json.load(open('data.json', 'r'))
+while word != '1234':
+    word = input('Enter word: ')
+    if word != '1234' and word in data:
+        print(data[word])
+    elif word != '1234' and word not in data:
+        print('Word not found. Try again.')
 
-
-def definition(entry):
-    return data[entry]
-
-
-word = input("Enter word: ")
-
-print(definition(word))
+print("Goodbye")
