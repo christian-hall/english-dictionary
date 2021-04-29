23660a7550a87712b0f0f6897e5f7c6c9ce25297
Christian Hall <christiannealhall@gmail.com>
2021-04-28 20:44:51 -0400

    added user response and autocorrect

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import json
+from difflib import get_close_matches as close_matches
 
 print('Welcome to the English Dictionary Python Program')
-print('Enter "1234" at any time to quit the program')
+print('Enter a blank at any time to quit the program')
 
-word, data = '', json.load(open('data.json', 'r'))
-while word != '1234':
-    word = input('Enter word: ')
-    if word != '1234' and word in data:
-        print(data[word])
-    elif word != '1234' and word not in data:
-        print('Word not found. Try again.')
+word, data = ' ', json.load(open('data.json', 'r'))
+while word != '':
+    word = input('Enter word: ').lower()
+    if word == '':
+        pass
+    elif word in data:
+        print(str(data[word]))
+    elif word not in data:
+        autocorrect = close_matches(word, data.keys())
+        if autocorrect:
+            for suggestion in autocorrect:
+                corrected_word = input(f'Did you mean "{suggestion}"? (y/n): ')
+                if corrected_word == 'y':
+                    print(str(data[suggestion]))
+                    break
+            else:
+                print('Word not found. Try again.')
+
+        else:
+            print('Word not found. Try again.')
 
 print("Goodbye")
